2ecb2a06623e17a5daf4e5ac23d68000be21ad7c
reklizyaka <ann.reklizon@gmail.com>
2020-02-09 13:37:52 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+function s(a,b) {
+  console.log(a + b + a);
+    return a + b + a;
+  }
+  function f(a, b, c) {
+    function sum(a, b) {
+      console.log(a + b);
+      return a + b;
+    }
+    
+          a = a || 2;
+        b = b || 3;
+    
+  if ( typeof c == "function"){
+      s(a,b);
+      return a + b + a;
+    } else {
+      sum(a,b);
+      return a + b;
+    }  
+   
+  }
+      f(3,5,s);
