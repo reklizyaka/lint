4ca85731235eaf773cb2b1f011f95c043bf5c079
reklizyaka <ann.reklizon@gmail.com>
2020-02-09 13:56:54 +0200

    1 problem

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
-function s(a,b) {
-  console.log(a + b + a);
+const fNum = 3;
+const sNum = 5;
+function s(a, b) {
     return a + b + a;
-  }
-  function f(a, b, c) {
-    function sum(a, b) {
-      console.log(a + b);
-      return a + b;
+}
+function f(a, b, c) {
+    function sum(d, e) {
+        return d + e;
     }
-    
-          a = a || 2;
-        b = b || 3;
-    
-  if ( typeof c == "function"){
-      s(a,b);
-      return a + b + a;
+    if (typeof c === 'function') {
+        s(a, b);
+        return a + b + a;
     } else {
-      sum(a,b);
-      console.log(a);
-      return a + b;
-    }  
-   
-  }
-      f(3,5,s);
+        sum(a, b);
+        return a + b;
+    }
+}
+f(fNum, sNum, s);
